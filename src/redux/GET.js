a57ca1d31ff9c82075a85c633a0120bffd5b0fcf
Veta Sheina <sssheina@gmail.com>
2023-06-08 11:10:54 +0300

    class GET console.log(r);

--- src/redux/GET.js
+++ src/redux/GET.js
@@ -2,6 +2,7 @@ class GET {
     static async getWords() {
         try {
             const r = await fetch('https://itgirlschool.justmakeit.ru/api/words');
+            console.log(r);
             const data = await r.json();
             return data;
         } catch (e) {
