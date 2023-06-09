54447c00fcca28368861386952322235ebf42800
Veta Sheina <sssheina@gmail.com>
2023-06-09 23:12:52 +0300

    /api/test/

--- src/redux/GET.js
+++ src/redux/GET.js
@@ -1,7 +1,7 @@
 class GET {
     static async getWords() {
         try {
-            const r = await fetch('https://itgirlschool.justmakeit.ru/api/words');
+            const r = await fetch('https://itgirlschool.justmakeit.ru/api/test/');
             console.log(r);
             const data = await r.json();
             return data;
@@ -12,3 +12,19 @@ class GET {
 }
 
 export default GET;
+
+
+// class GET {
+//     static async getWords() {
+//         try {
+//             const r = await fetch('http://itgirlschool.justmakeit.ru/api/words');
+//             console.log(r);
+//             const data = await r.json();
+//             return data;
+//         } catch (e) {
+//             console.error(e);
+//         }
+//     }
+// }
+
+// export default GET;
